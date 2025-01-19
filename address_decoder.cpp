0b90fafb9c514ff1ce7f036ddf16b31981ad4775
Vikram Mullick <vikram1.mullick@gmail.com>
2025-01-19 18:08:02 -0500

    refactor address decoder

--- address_decoder.cpp
+++ address_decoder.cpp
@@ -4,26 +4,34 @@
 address_decoder_t::address_decoder_t(bus_t &bus) : m_bus(bus) {}
 
 void address_decoder_t::select_chip() {
-    // TODO: switch to switch case
-    if (m_bus.m_addr_line == constants::KEYBOARD_ADDR_LO ||
-        m_bus.m_addr_line == constants::KEYBOARD_ADDR_HI ||
-        m_bus.m_addr_line == constants::KEYBOARD_WAIT_REL_ADDR) {
-        m_bus.m_chip_select = 1 << constants::KEYBOARD_CHIP_SELECT;
-    } else if (m_bus.m_addr_line == constants::DELAY_TIMER_ADDR) {
-        m_bus.m_chip_select = 1 << constants::DELAY_TIMER_CHIP_SELECT;
-    } else if (m_bus.m_addr_line == constants::SOUND_TIMER_ADDR) {
-        m_bus.m_chip_select = 1 << constants::SOUND_TIMER_CHIP_SELECT;
-    } else if (m_bus.m_addr_line == constants::PPU_CLEAR_OR_READ_VBLANK_ADDR ||
-               m_bus.m_addr_line == constants::PPU_SPRITE_X_ADDR ||
-               m_bus.m_addr_line == constants::PPU_SPRITE_Y_ADDR ||
-               m_bus.m_addr_line == constants::PPU_SPRITE_ADDR_LO ||
-               m_bus.m_addr_line == constants::PPU_SPRITE_ADDR_HI ||
-               m_bus.m_addr_line ==
-                   constants::PPU_DRAW_SPRITE_OR_READ_TOGGLED_OFF_ADDR) {
-        m_bus.m_chip_select = 1 << constants::PPU_CHIP_SELECT;
-    } else if (m_bus.m_addr_line == constants::RNG_ADDR) {
-        m_bus.m_chip_select = 1 << constants::RNG_CHIP_SELECT;
-    } else {
-        m_bus.m_chip_select = 1 << constants::MEMORY_CHIP_SELECT;
+    uint8_t chip_select_shift;
+
+    switch (m_bus.m_addr_line) {
+    case constants::KEYBOARD_ADDR_LO:
+    case constants::KEYBOARD_ADDR_HI:
+    case constants::KEYBOARD_WAIT_REL_ADDR:
+        chip_select_shift = constants::KEYBOARD_CHIP_SELECT;
+        break;
+    case constants::DELAY_TIMER_ADDR:
+        chip_select_shift = constants::DELAY_TIMER_CHIP_SELECT;
+        break;
+    case constants::SOUND_TIMER_ADDR:
+        chip_select_shift = constants::SOUND_TIMER_CHIP_SELECT;
+        break;
+    case constants::PPU_CLEAR_OR_READ_VBLANK_ADDR:
+    case constants::PPU_SPRITE_X_ADDR:
+    case constants::PPU_SPRITE_Y_ADDR:
+    case constants::PPU_SPRITE_ADDR_LO:
+    case constants::PPU_SPRITE_ADDR_HI:
+    case constants::PPU_DRAW_SPRITE_OR_READ_TOGGLED_OFF_ADDR:
+        chip_select_shift = constants::PPU_CHIP_SELECT;
+        break;
+    case constants::RNG_ADDR:
+        chip_select_shift = constants::RNG_CHIP_SELECT;
+        break;
+    default:
+        chip_select_shift = constants::MEMORY_CHIP_SELECT;
     }
+
+    m_bus.m_chip_select = 1 << chip_select_shift;
 }
